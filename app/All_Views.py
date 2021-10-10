1d24183dc0c1af034550c48404293adfafef219d
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-10-10 01:31:23 -0500

    db first test

--- app/All_Views.py
+++ app/All_Views.py
@@ -1,14 +1,15 @@
 from app import app
 from datetime import datetime
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+
 # contendra todas nuestra vistas
 # add data base
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://users.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password123@localhost/users"
 app.config["SECRET_KEY"] = "contraseña123"
 # initialize the database
 db = SQLAlchemy(app)
@@ -28,20 +29,30 @@ class Users(db.Model):
 
 
 # create a form class
-# def userForm(FlaskForm):
-#     name = StringField("Name", validators=[DataRequired()])  # imput box
-#     submit = SubmitField("Submit")
+class userForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])  # imput box
+    email = StringField("Email", validators=[DataRequired()])  # imput box
+    submit = SubmitField("Submit")
 
 
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-#     name = None
-#     form = userForm()
-#     # validate form
-#     if form.validate_on_submit():
-#         name = form.name.data
-#         form.name.data = ""  # se limpia para la proxima vez
-#     return render_template("sing_up.html", name=name, form=form)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    name = None
+    email = None
+    form = userForm()
+    # validate form
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data)
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ""  # se limpia para la proxima vez
+        form.email.data = ""
+    return render_template("public/sign_up.html", name=name, form=form)
+
+
 def sing_up():
     # get to get the content and post to post it into our server
     # this code wiil execute only when the route recieve a post request
