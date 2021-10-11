71975117d6b6c8e61bc8d75f92df1d77b1f6e28f
Santiago Urrego <santi.roymer@gmail.com>
2021-10-11 08:36:59 -0500

    Actualización pendiente del 10/10/2021

--- app/All_Views.py
+++ app/All_Views.py
@@ -1,10 +1,10 @@
 from app import app
 from datetime import datetime
 from flask import render_template, request, redirect, flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField
+# from wtforms.validators import DataRequired
 
 
 # contendra todas nuestra vistas
@@ -12,59 +12,59 @@ from wtforms.validators import DataRequired
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password123@localhost/users"
 app.config["SECRET_KEY"] = "contraseña123"
 # initialize the database
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-# create the model
-
-
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(13), nullable=False)
-    email = db.Column(db.String(13), nullable=False, unique=True)
-    date_added = db.Column(db.DateTime, default=datetime.utcnow)
-
-    # Create a string
-    def __repr__(self) -> str:
-        return "<Name %r>" % self.name
+# # create the model
 
 
-# create a form class
-class userForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])  # imput box
-    email = StringField("Email", validators=[DataRequired()])  # imput box
-    submit = SubmitField("Submit")
+# class Users(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(13), nullable=False)
+#     email = db.Column(db.String(13), nullable=False, unique=True)
+#     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+#     # Create a string
+#     def __repr__(self) -> str:
+#         return "<Name %r>" % self.name
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    name = None
-    email = None
-    form = userForm()
-    # validate form
-    if form.validate_on_submit():
-        user = Users.query.filter_by(email=form.email.data)
-        if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
-            db.session.add(user)
-            db.session.commit()
-        name = form.name.data
-        form.name.data = ""  # se limpia para la proxima vez
-        form.email.data = ""
-    return render_template("public/sign_up.html", name=name, form=form)
+# # create a form class
+# class userForm(FlaskForm):
+#     name = StringField("Name", validators=[DataRequired()])  # imput box
+#     email = StringField("Email", validators=[DataRequired()])  # imput box
+#     submit = SubmitField("Submit")
 
 
-def sing_up():
-    # get to get the content and post to post it into our server
-    # this code wiil execute only when the route recieve a post request
-    if request.method == "POST":
-        req = request.form
-        # here we have several alternative to store what's in the form inside variables:
-        username = req["username"]
-        email = req.get("email")
-        password = request.form["password"]
-        print(username, email, password)
-        return redirect(request.url)
-    return render_template("public/sign_up.html")
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     name = None
+#     email = None
+#     form = userForm()
+#     # validate form
+#     if form.validate_on_submit():
+#         user = Users.query.filter_by(email=form.email.data)
+#         if user is None:
+#             user = Users(name=form.name.data, email=form.email.data)
+#             db.session.add(user)
+#             db.session.commit()
+#         name = form.name.data
+#         form.name.data = ""  # se limpia para la proxima vez
+#         form.email.data = ""
+#     return render_template("public/sign_up.html", name=name, form=form)
+
+
+# def sing_up():
+#     # get to get the content and post to post it into our server
+#     # this code wiil execute only when the route recieve a post request
+#     if request.method == "POST":
+#         req = request.form
+#         # here we have several alternative to store what's in the form inside variables:
+#         username = req["username"]
+#         email = req.get("email")
+#         password = request.form["password"]
+#         print(username, email, password)
+#         return redirect(request.url)
+#     return render_template("public/sign_up.html")
 
 
 @app.route("/")
