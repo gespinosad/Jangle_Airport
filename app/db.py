519c96a3d18affe31ac4c533ffb6ec42090407cf
BrayanDuqueC <bjduque.fesc@gmail.com>
2021-10-21 00:13:50 -0500

    Cambios env y home user

--- app/db.py
+++ app/db.py
@@ -4,5 +4,5 @@ import pymysql
 def obtener_conexion():
     return pymysql.connect(host='localhost',  # probar con su usuario de sistema o localhost
                                 user='root',  # probar con root
-                                password='3535',
-                                db='jungle_airport')
+                                password='Hinokami2027.',
+                                db='jangleairport')
